b9501627384835fbc2c315487eb97ce718e1fe29
HooN <hoon@nhnnext.org>
2015-07-25 18:09:04 +0900

    #6, locked screen rotation. (Some modified code was omitted by mistake.)

--- app/src/main/java/com/example/hoon/gloriousfamily/CharacterSelectActivity.java
+++ app/src/main/java/com/example/hoon/gloriousfamily/CharacterSelectActivity.java
@@ -1,7 +1,8 @@
 package com.example.hoon.gloriousfamily;
 
-import android.content.Intent;
 import android.support.v4.app.FragmentActivity;
+import android.content.pm.ActivityInfo;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.*;
 import android.widget.ImageView;
@@ -11,5 +12,6 @@ public class CharacterSelectActivity extends FragmentActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_character_select);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
     }
 }
--- app/src/main/java/com/example/hoon/gloriousfamily/ProfileActivity.java
+++ app/src/main/java/com/example/hoon/gloriousfamily/ProfileActivity.java
@@ -1,7 +1,8 @@
 package com.example.hoon.gloriousfamily;
 
-import android.content.Intent;
 import android.support.v4.app.FragmentActivity;
+import android.content.pm.ActivityInfo;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.*;
 import android.widget.ImageView;
@@ -11,12 +12,13 @@ public class ProfileActivity extends FragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
     }
 
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.button_getstarted:
-                // startActivity(new Intent(this, CharacterSelectActivity.class));
+                startActivity(new Intent(this, CharacterSelectActivity.class));
                 break;
         }
     }
