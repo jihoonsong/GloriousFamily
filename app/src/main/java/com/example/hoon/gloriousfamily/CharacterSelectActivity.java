60cca089054df60631dde358e26eb6b12d94b185
HooN <hoon@nhnnext.org>
2015-07-25 11:30:23 +0900

    #5, added activity_character_select.

---
+++ app/src/main/java/com/example/hoon/gloriousfamily/CharacterSelectActivity.java
@@ -0,0 +1,15 @@
+package com.example.hoon.gloriousfamily;
+
+import android.content.Intent;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.view.*;
+import android.widget.ImageView;
+
+public class CharacterSelectActivity extends FragmentActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_character_select);
+    }
+}
