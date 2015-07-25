686cf9c28559b72048e2caed81591eabcf04a3f1
HooN <hoon@nhnnext.org>
2015-07-26 08:30:53 +0900

    #14, added activity_birth.

---
+++ app/src/main/java/com/example/hoon/gloriousfamily/BirthActivity.java
@@ -0,0 +1,23 @@
+package com.example.hoon.gloriousfamily;
+
+import android.app.Activity;
+import android.content.pm.ActivityInfo;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.*;
+import android.widget.ImageView;
+
+public class BirthActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_birth);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+    }
+
+    public void onClick(View v) {
+        switch (v.getId()) {
+        }
+    }
+}
