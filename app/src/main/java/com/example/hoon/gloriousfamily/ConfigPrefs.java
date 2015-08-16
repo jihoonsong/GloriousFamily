195d2c8423843bf018efc15ff80405dc91c6d7af
HooN <hoon@nhnnext.org>
2015-08-16 21:03:20 +0900

    #59, implementing ProfileActivity.

---
+++ app/src/main/java/com/example/hoon/gloriousfamily/ConfigPrefs.java
@@ -0,0 +1,8 @@
+package com.example.hoon.gloriousfamily;
+
+import org.androidannotations.annotations.sharedpreferences.SharedPref;
+
+@SharedPref(value=SharedPref.Scope.APPLICATION_DEFAULT)
+public interface ConfigPrefs {
+
+}
--- app/src/main/java/com/example/hoon/gloriousfamily/ProfileActivity.java
+++ app/src/main/java/com/example/hoon/gloriousfamily/ProfileActivity.java
@@ -1,9 +1,50 @@
 package com.example.hoon.gloriousfamily;
 
 import android.app.Activity;
+import android.content.pm.ActivityInfo;
+import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
+
+import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.ViewById;
+import org.androidannotations.annotations.sharedpreferences.Pref;
 
 @EActivity(R.layout.activity_profile)
 public class ProfileActivity extends Activity {
+  @ViewById(R.id.button_start)
+  ImageView buttonStart;
+
+  @Pref
+  StartFlagsPrefs_ startFlagsPrefs;
+
+  @Pref
+  ConfigPrefs_ configPrefs;
+
+  @AfterViews
+  protected void init() {
+    // Lock rotation.
+    setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+    // Load images.
+    Glide.with(getApplicationContext()).load(R.drawable.button_select).into(buttonStart);
+  }
+
+  @Click(R.id.button_start)
+  protected void buttonStartClicked() {
+    // TODO : Store received input and check it's validity.
+
+    // TODO : If input is valid, do belows.
+    // Set profile flag true.
+    startFlagsPrefs.isProfile().put(true);
+
+    // Swap to CharacterSelectActivity.
+    CharacterSelectActivity_.intent(this).start();
+
+    overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+    finish();
+    System.gc();
+  }
 }
