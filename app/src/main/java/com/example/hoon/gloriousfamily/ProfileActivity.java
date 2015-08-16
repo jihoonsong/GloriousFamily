fa35a8fd35ee426c1c96fba24852e2b8f2e39510
HooN <hoon@nhnnext.org>
2015-08-17 08:04:19 +0900

    #66, customizing radio button.

--- app/src/main/java/com/example/hoon/gloriousfamily/ProfileActivity.java
+++ app/src/main/java/com/example/hoon/gloriousfamily/ProfileActivity.java
@@ -6,7 +6,6 @@ import android.graphics.Typeface;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.RadioButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -32,11 +31,23 @@ public class ProfileActivity extends Activity {
   @ViewById(R.id.text_user_gender)
   TextView textUserGender;
 
-  @ViewById(R.id.user_male)
-  RadioButton userMale;
+  @ViewById(R.id.button_user_male_selected)
+  ImageView buttonUserMaleSelected;
 
-  @ViewById(R.id.user_female)
-  RadioButton userFemale;
+  @ViewById(R.id.button_user_male_unselected)
+  ImageView buttonUserMaleUnselected;
+
+  @ViewById(R.id.button_user_female_selected)
+  ImageView buttonUserFemaleSelected;
+
+  @ViewById(R.id.button_user_female_unselected)
+  ImageView buttonUserFemaleUnselected;
+
+  @ViewById(R.id.text_user_male)
+  TextView textUserMale;
+
+  @ViewById(R.id.text_user_female)
+  TextView textUserFemale;
 
   @ViewById(R.id.text_character_name)
   TextView textCharacterName;
@@ -47,11 +58,23 @@ public class ProfileActivity extends Activity {
   @ViewById(R.id.text_character_gender)
   TextView textCharacterGender;
 
-  @ViewById(R.id.character_male)
-  RadioButton characterMale;
+  @ViewById(R.id.button_character_male_selected)
+  ImageView buttonCharacterMaleSelected;
 
-  @ViewById(R.id.character_female)
-  RadioButton characterFemale;
+  @ViewById(R.id.button_character_male_unselected)
+  ImageView buttonCharacterMaleUnselected;
+
+  @ViewById(R.id.button_character_female_selected)
+  ImageView buttonCharacterFemaleSelected;
+
+  @ViewById(R.id.button_character_female_unselected)
+  ImageView buttonCharacterFemaleUnselected;
+
+  @ViewById(R.id.text_character_male)
+  TextView textCharacterMale;
+
+  @ViewById(R.id.text_character_female)
+  TextView textCharacterFemale;
 
   @Pref
   StartFlagsPrefs_ startFlagsPrefs;
@@ -60,6 +83,8 @@ public class ProfileActivity extends Activity {
   ConfigPrefs_ configPrefs;
 
   private String mode = "";
+  private String userGender = "MALE";
+  private String characterGender = "";
 
   @AfterViews
   protected void init() {
@@ -71,23 +96,67 @@ public class ProfileActivity extends Activity {
 
     // Check mode.
     mode = configPrefs.Mode().get();
-    if(mode.equals("B") || mode.equals("D")) {
-      textCharacterName.setVisibility(View.INVISIBLE);
-      editCharacterName.setVisibility(View.INVISIBLE);
-      textCharacterGender.setVisibility(View.INVISIBLE);
-      characterMale.setVisibility(View.INVISIBLE);
-      characterFemale.setVisibility(View.INVISIBLE);
+    if(mode.equals("A") || mode.equals("C")) {
+      textCharacterName.setVisibility(View.VISIBLE);
+      editCharacterName.setVisibility(View.VISIBLE);
+
+      characterGender = "MALE";
+      buttonCharacterMaleSelected.setVisibility(View.VISIBLE);
+      buttonCharacterFemaleUnselected.setVisibility(View.VISIBLE);
+      textCharacterGender.setVisibility(View.VISIBLE);
+      textCharacterMale.setVisibility(View.VISIBLE);
+      textCharacterFemale.setVisibility(View.VISIBLE);
     }
 
     // Set fonts types.
     textUserName.setTypeface(Typeface.createFromAsset(getAssets(), "NotoSansCJKkr_Medium.otf"));
     textUserGender.setTypeface(Typeface.createFromAsset(getAssets(), "NotoSansCJKkr_Medium.otf"));
+    textUserMale.setTypeface(Typeface.createFromAsset(getAssets(), "NotoSansCJKkr_Regular.otf"));
+    textUserFemale.setTypeface(Typeface.createFromAsset(getAssets(), "NotoSansCJKkr_Regular.otf"));
     textCharacterName.setTypeface(Typeface.createFromAsset(getAssets(), "NotoSansCJKkr_Medium.otf"));
     textCharacterGender.setTypeface(Typeface.createFromAsset(getAssets(), "NotoSansCJKkr_Medium.otf"));
-    userMale.setTypeface(Typeface.createFromAsset(getAssets(), "NotoSansCJKkr_Regular.otf"));
-    userFemale.setTypeface(Typeface.createFromAsset(getAssets(), "NotoSansCJKkr_Regular.otf"));
-    characterMale.setTypeface(Typeface.createFromAsset(getAssets(), "NotoSansCJKkr_Regular.otf"));
-    characterFemale.setTypeface(Typeface.createFromAsset(getAssets(), "NotoSansCJKkr_Regular.otf"));
+    textCharacterMale.setTypeface(Typeface.createFromAsset(getAssets(), "NotoSansCJKkr_Regular.otf"));
+    textCharacterFemale.setTypeface(Typeface.createFromAsset(getAssets(), "NotoSansCJKkr_Regular.otf"));
+  }
+
+  @Click(R.id.button_user_male_unselected)
+  protected void buttonUserMaleUnselectedClicked() {
+    userGender = "MALE";
+
+    buttonUserFemaleSelected.setVisibility(View.INVISIBLE);
+    buttonUserFemaleUnselected.setVisibility(View.VISIBLE);
+    buttonUserMaleUnselected.setVisibility(View.INVISIBLE);
+    buttonUserMaleSelected.setVisibility(View.VISIBLE);
+  }
+
+  @Click(R.id.button_user_female_unselected)
+  protected void buttonUserFemaleUnselectedClicked() {
+    userGender = "FEMALE";
+
+    buttonUserMaleSelected.setVisibility(View.INVISIBLE);
+    buttonUserMaleUnselected.setVisibility(View.VISIBLE);
+    buttonUserFemaleUnselected.setVisibility(View.INVISIBLE);
+    buttonUserFemaleSelected.setVisibility(View.VISIBLE);
+  }
+
+  @Click(R.id.button_character_male_unselected)
+  protected void buttonCharacterMaleUnselectedClicked() {
+    characterGender = "MALE";
+
+    buttonCharacterFemaleSelected.setVisibility(View.INVISIBLE);
+    buttonCharacterFemaleUnselected.setVisibility(View.VISIBLE);
+    buttonCharacterMaleUnselected.setVisibility(View.INVISIBLE);
+    buttonCharacterMaleSelected.setVisibility(View.VISIBLE);
+  }
+
+  @Click(R.id.button_character_female_unselected)
+  protected void buttonCharacterFemaleUnselectedClicked() {
+    characterGender = "FEMALE";
+
+    buttonCharacterMaleSelected.setVisibility(View.INVISIBLE);
+    buttonCharacterMaleUnselected.setVisibility(View.VISIBLE);
+    buttonCharacterFemaleUnselected.setVisibility(View.INVISIBLE);
+    buttonCharacterFemaleSelected.setVisibility(View.VISIBLE);
   }
 
   @Click(R.id.button_get_started)
@@ -106,22 +175,6 @@ public class ProfileActivity extends Activity {
         Toast.makeText(this, "Please write your character's name.", Toast.LENGTH_SHORT).show();
         return;
       }
-    }
-
-    String userGender = "";
-    if(userMale.isChecked()) {
-      userGender = "MALE";
-    }
-    else {
-      userGender = "FEMALE";
-    }
-
-    String characterGender = "";
-    if(characterMale.isChecked()) {
-      characterGender = "MALE";
-    }
-    else {
-      characterGender = "FEMALE";
     }
 
     // Store inputs.
